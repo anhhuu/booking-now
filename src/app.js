12b5e8f15f08db157e4760b14f627c806df4f5db
anhhuu <panhhuu@gmail.com>
2020-12-15 22:02:16 +0700

    fix debug

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const debugHttp = require('debug')('booking-now:http')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
