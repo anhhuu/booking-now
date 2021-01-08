bdde72cb5e8b1a8e73a3b2134b5a881072cef0fd
Lê Danh Lưu <danhluu1@gmail.com>
2021-01-08 14:00:43 +0700

    done passport final

--- src/views/html/res/js/login-register.js
+++ src/views/html/res/js/login-register.js
@@ -39,21 +39,22 @@ function openRegisterModal() {
 }
 
 function loginAjax() {
-    // $.post("/login", function(data) {
-    //     if (data == 1) {
-    //         window.location.replace("/");
-    //     } else {
-    //         shakeModal();
-    //     }
-    // });
+    $.post("/login", function(data) {
+        if (data == 1) {
+            window.location.replace("/");
+        } else {
+            shakeModal();
+        }
+    });
 
-    // /*   Simulate error message from the server   */
-    // shakeModal();
+    /*   Simulate error message from the server   */
+    shakeModal();
 }
 
 function shakeModal() {
     $('#loginModal .modal-dialog').addClass('shake');
     $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+    $('input[type="password"]').val('');
     setTimeout(function() {
         $('#loginModal .modal-dialog').removeClass('shake');
     }, 1000);
