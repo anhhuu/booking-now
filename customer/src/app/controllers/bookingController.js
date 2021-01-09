a8ae7f9c667be76b13f8b139183fef3f89535d3b
Lê Danh Lưu <danhluu1@gmail.com>
2021-01-09 22:50:46 +0700

    add  bookinghistory.ejs

--- customer/src/app/controllers/bookingController.js
+++ customer/src/app/controllers/bookingController.js
@@ -3,3 +3,6 @@ const bookingService = require('../models/services/bookingService');
 module.exports.save = (req, res, next) => {
     res.send(req.body);
 }
+module.exports.index = (req, res, next) => {
+    res.render('bookingHistory')
+}
--- customer/src/routes/bookings.js
+++ customer/src/routes/bookings.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const bookingController = require('../app/controllers/bookingController')
 
 router.post('/create', bookingController.save)
+router.get('/history', bookingController.index)
 module.exports = router;
